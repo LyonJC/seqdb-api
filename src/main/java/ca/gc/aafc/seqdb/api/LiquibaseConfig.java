f2510c93dcd1822ebab44c85b51e9f950adb7a07
Brandon Andre <andr0284@algonquinlive.com>
2019-08-16 12:02:40 -0400

    In Progress - Support #16965 - Change default Liquibase Datasource connection pool size
    https://redmine.biodiversity.agr.gc.ca/issues/16965
    
    - Switched to using the LiquibaseDataSource annotation. Still working
    out issues with H2.

--- src/main/java/ca/gc/aafc/seqdb/api/LiquibaseConfig.java
+++ src/main/java/ca/gc/aafc/seqdb/api/LiquibaseConfig.java
@@ -1,19 +1,18 @@
 package ca.gc.aafc.seqdb.api;
 
-import javax.inject.Inject;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.zaxxer.hikari.HikariDataSource;
-
-import liquibase.integration.spring.SpringLiquibase;
 
 /**
  * Liquibase configurations. Currently this class is responsible for changing the number of
@@ -25,41 +24,39 @@ import liquibase.integration.spring.SpringLiquibase;
 @Configuration
 @EnableConfigurationProperties({LiquibaseProperties.class, DataSourceProperties.class})
 public class LiquibaseConfig {
- 
-  @Value("${spring.liquibase.maximumPoolSize:2}")
-  private int maximumPoolSize;
   
-  @Inject
+  private static final int LIQUIBASE_POOL_SIZE = 2;
+  
+  @Autowired
+  private LiquibaseProperties liquibaseProperties;
+  
+  @Autowired
   private DataSourceProperties dataSourceProperties;
   
-  /**
-   * Reconfigure the SpringLiquibase bean to set the connection pool size to a smaller number.
-   * 
-   * @param dataSource
-   *          DataSource to retrieve the URL from.
-   * @param liquibaseProperties
-   *          Liquibase Properties to retrieve the user and password from.
-   * @return Reconfigured SpringLiquibase bean.
-   */
   @Bean
-  public SpringLiquibase liquibase(DataSource dataSource, LiquibaseProperties liquibaseProperties) {
-
-    DataSource liquibaseDataSource = DataSourceBuilder.create()
-        .username(liquibaseProperties.getUser())
-        .password(liquibaseProperties.getPassword())
-        .url(dataSourceProperties.getUrl())
-        .build();
-    
-    // Change the pool size of the liquibase connection pool.
-    if (liquibaseDataSource instanceof HikariDataSource) {
-      ((HikariDataSource) liquibaseDataSource).setMaximumPoolSize(maximumPoolSize);
-    }
-
-    SpringLiquibase liquibase = new SpringLiquibase();
-    liquibase.setChangeLog(liquibaseProperties.getChangeLog());
-    liquibase.setDataSource(liquibaseDataSource);
-    
-    return liquibase;
+  @Primary
+  public DataSource dataSource() {
+      return DataSourceBuilder.create()
+              .username(dataSourceProperties.getUsername())
+              .password(dataSourceProperties.getPassword())
+              .url(dataSourceProperties.getUrl())
+              .build();
   }
 
+  @LiquibaseDataSource
+  @Bean
+  public DataSource liquibaseDataSource() {
+      DataSource ds = DataSourceBuilder.create()
+              .username(liquibaseProperties.getUser())
+              .password(liquibaseProperties.getPassword())
+              .url(dataSourceProperties.getUrl())
+              .build();
+
+      if (ds instanceof HikariDataSource) {
+          ((HikariDataSource) ds).setMaximumPoolSize(LIQUIBASE_POOL_SIZE);
+      }
+
+      return ds;
+  }
+  
 }
